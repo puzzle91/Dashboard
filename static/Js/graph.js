7decadb435fedd0e3a4e5c0612fdb137a3150cab
Eduardo <eduardop@tcd.ie>
2017-07-03 14:55:57 +0100

    Added Graphs and CSS

--- static/Js/graph.js
+++ static/Js/graph.js
@@ -131,22 +131,9 @@ var AreaChart = dc.pieChart("#area-chart")
        .group(AttackperArea);
 
 
-// var timeChart = dc.barChart("#time-chart")       
-
-//        timeChart
-//        .width(800)
-//        .height(200)
-//        .margins({top: 10, right: 50, bottom: 30, left: 50})
-//        .dimension(dateDim)
-//        .group(AttacksByDate)
-//        .transitionDuration(500)
-//        .x(d3.time.scale().domain([minDate, maxDate]))
-//        .elasticY(true)
-//        .xAxisLabel("Year")
-//        .yAxis().ticks(4);
 
 var YearChart = dc.lineChart("#years-chart")  
-
+    // debugger;
     YearChart
     .width(1400)
        .height(400)
@@ -165,71 +152,117 @@ var YearChart = dc.lineChart("#years-chart")
        .group(numofattacks);
 
 
+// var surfAttacksByCountry = countryDim.group().reduceSum(function (d) {
+//     if (d.Activity === 'Surfing' && d.Country === "USA", "AUSTRALIA", "SOUTH AFRICA") {
+//         return  1;
+//     } else {
+//         return 0
+//     }
+// });
+
+// var swimAttacksByCountry = countryDim.group().reduceSum(function (d) {
+//     if (d.Activity === 'Swimming' && d.Country === "USA", "AUSTRALIA", "SOUTH AFRICA") {
+//         return  1;
+//     } else {
+//         return 0
+//     }
+// });
+
+// var fishAttacksByCountry = countryDim.group().reduceSum(function (d) {
+//     if (d.Activity === 'Fishing' && d.Country === "USA", "AUSTRALIA", "SOUTH AFRICA") {
+//         return  1;
+//     } else {
+//         return 0
+//     }
+// });
+
+// var otherAttacksByCountry = countryDim.group().reduceSum(function (d) {
+//     if (d.Activity.indexOf ['Surfing', 'Swimming', 'Fishing'] > -1 ) {
+//         return  1;
+//     } else {
+//         return 0
+//     }
+// });
 
 
-var attacksPerYearByCountryChart = dc.compositeChart("#attacks-per-year");
 
 
-var usaAttacksPerYear = YearsDim.group().reduceSum(function (d){
-    if (d.Country === 'USA'){
-        return 1;
-    } else {
-        return 0;
-    }
-});
-var ausAttacksPerYear = YearsDim.group().reduceSum(function (d){
-    if (d.Country === 'AUSTRALIA'){
-        return 1;
-    } else {
-        return 0;
-    }
-});
-var saAttacksPerYear = YearsDim.group().reduceSum(function (d){
-    if (d.Country === 'SOUTH AFRICA'){
-        return 1;
-    } else {
-        return 0;
-    }
-});
-var otherAttacksPerYear = YearsDim.group().reduceSum(function (d){
-    if (d.Country.indexOf ['SOUTH AFRICA', 'AUSTRALIA', 'USA'] > -1 ){
-        return 0;
-    } else {
-        return 1;
-    }
-});
 
-attacksPerYearByCountryChart
-    .width(1400)
-    .height(200)
-    .margins({top:10, right: 50, bottom:80, left: 50})
-    .x(d3.time.scale().domain([2006,2016]))
-    /*.xUnits(dc.units.ordinal)*/
-    .elasticY(true)
-    .yAxisLabel("Num of Attacks")
-    .xAxisLabel("Year")
-    .legend(dc.legend().x(80).y(120).itemHeight(13).gap(5))
-    .renderHorizontalGridLines(true)
-    .compose([
-            dc.lineChart(attacksPerYearByCountryChart)
-                .dimension(YearsDim)
-                .colors('green')
-                .group(usaAttacksPerYear, 'USA'),
-            dc.lineChart(attacksPerYearByCountryChart)
-                .dimension(YearsDim)
-                .colors('red')
-                .group(ausAttacksPerYear, 'AUSTRALIA'),
-            dc.lineChart(attacksPerYearByCountryChart)
-                .dimension(YearsDim)
-                .colors('blue')
-                .group(saAttacksPerYear, 'SOUTH AFRICA'),
-            dc.lineChart(attacksPerYearByCountryChart)
-                .dimension(YearsDim)
-                .colors('black')
-                .group(otherAttacksPerYear, 'OTHER')
-        ])
-    .brushOn(true);
 
+
+
+
+
+// var attacksPerYearByCountryChart = dc.compositeChart("#attacks-per-year");
+
+
+// var usaAttacksPerYear = YearsDim.group().reduceSum(function (d){
+//     if (d.Country === 'USA'){
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// });
+// var ausAttacksPerYear = YearsDim.group().reduceSum(function (d){
+//     if (d.Country === 'AUSTRALIA'){
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// });
+// var saAttacksPerYear = YearsDim.group().reduceSum(function (d){
+//     if (d.Country === 'SOUTH AFRICA'){
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// });
+// var otherAttacksPerYear = YearsDim.group().reduceSum(function (d){
+//     if (d.Country.indexOf ['SOUTH AFRICA', 'AUSTRALIA', 'USA'] > -1 ){
+//         return 0;
+//     } else {
+//         return 1;
+//     }
+// });
+
+// attacksPerYearByCountryChart
+//     .width(1400)
+//     .height(200)
+//     .margins({top:10, right: 50, bottom:80, left: 50})
+//     .x(d3.time.scale().domain([2006,2016]))
+//     /*.xUnits(dc.units.ordinal)*/
+//     .elasticY(true)
+//     .yAxisLabel("Num of Attacks")
+//     .xAxisLabel("Year")
+//     .legend(dc.legend().x(80).y(120).itemHeight(13).gap(5))
+//     .renderHorizontalGridLines(true)
+//     .compose([
+//             dc.lineChart(attacksPerYearByCountryChart)
+//                 .dimension(YearsDim)
+//                 .colors('green')
+//                 .group(usaAttacksPerYear, 'USA'),
+//             dc.lineChart(attacksPerYearByCountryChart)
+//                 .dimension(YearsDim)
+//                 .colors('red')
+//                 .group(ausAttacksPerYear, 'AUSTRALIA'),
+//             dc.lineChart(attacksPerYearByCountryChart)
+//                 .dimension(YearsDim)
+//                 .colors('blue')
+//                 .group(saAttacksPerYear, 'SOUTH AFRICA'),
+//             dc.lineChart(attacksPerYearByCountryChart)
+//                 .dimension(YearsDim)
+//                 .colors('black')
+//                 .group(otherAttacksPerYear, 'OTHER')
+//         ])
+//     .brushOn(true);
+
+
+numberAttacksND
+       .formatNumber(d3.format("d"))
+       .valueAccessor(function (d) {
+           return d;
+       })
+       .group(all);
  
    dc.renderAll();
 }
